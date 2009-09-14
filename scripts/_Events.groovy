a1f1976f0636b1c93b45d50e46763bd3490a6fd8
Peter Ledbrook <peter@cacoethes.co.uk>
2009-09-14 15:31:28 +0100

    The Selenium server is now loaded into its own class loader that has nothing on the classpath other than the JDK and the selenium-server.jar file. This is necessary because that JAR file contains the classes from several libraries, some of which conflict with Grails dependencies.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,3 +1,5 @@
+import java.net.URLClassLoader
+
 eventAllTestsStart = {
 	if (binding.variables.containsKey("functionalTests")) {
 		functionalTests << "selenium"
@@ -13,13 +15,33 @@ eventTestSuiteStart = {String type ->
 		event("StatusUpdate", ["selenium config: $seleniumConfig"])
 
 		event("StatusUpdate", ["starting selenium server"])
-		def conf = new org.openqa.selenium.server.RemoteControlConfiguration()
+		
+		// The Selenium server needs to be loaded into a clean class
+		// loader because the "selenium-server.jar" includes its own
+		// dependencies which conflict with some of the Grails ones.
+		def serverClassLoader = new URLClassLoader(
+				[ new File("${seleniumRcPluginDir}/lib/server/selenium-server.jar").toURI().toURL() ] as URL[],
+			    ClassLoader.systemClassLoader)
+
+		// HACK - It seems that Jetty will load classes using the thread
+		// context class loader, so we temporarily make the server class#
+		// loader the context CL.
+		def oldContextCL = Thread.currentThread().contextClassLoader
+		Thread.currentThread().contextClassLoader = serverClassLoader
+
+		// Create a configuration and start the server.
+		def conf = serverClassLoader.loadClass("org.openqa.selenium.server.RemoteControlConfiguration").newInstance()
 		conf.port = seleniumConfig.selenium.port
 		conf.singleWindow = seleniumConfig.selenium.singleWindow
-		seleniumServer = new org.openqa.selenium.server.SeleniumServer(seleniumConfig.selenium.slowResources, conf)
+		seleniumServer = serverClassLoader.loadClass("org.openqa.selenium.server.SeleniumServer").newInstance(seleniumConfig.selenium.slowResources, conf)
 		seleniumServer.start()
 
 		event("StatusUpdate", ["starting selenium instance"])
+		
+		// Jetty is now listening on separate threads, so it appears
+		// to be safe to reset the context class loader for this thread.
+		// That's the theory anyway.
+		Thread.currentThread().contextClassLoader = oldContextCL
 
 		com.energizedwork.grails.plugins.seleniumrc.GrailsSeleneseTestCase.selenium = new com.thoughtworks.selenium.GroovySelenium(new com.thoughtworks.selenium.DefaultSelenium(seleniumConfig.selenium.host, seleniumConfig.selenium.port, seleniumConfig.selenium.browser, seleniumConfig.selenium.url ?: config.grails.serverURL))
 		com.energizedwork.grails.plugins.seleniumrc.GrailsSeleneseTestCase.selenium.start()
