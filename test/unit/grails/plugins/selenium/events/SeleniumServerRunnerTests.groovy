2276fe1c0f66133b8b009496cfcefa861222923b
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-04-01 23:01:28 +0100

    properly test that selenium server is started and stopped

--- test/unit/grails/plugins/selenium/events/SeleniumServerRunnerTests.groovy
+++ test/unit/grails/plugins/selenium/events/SeleniumServerRunnerTests.groovy
@@ -5,7 +5,10 @@ import grails.plugins.selenium.SeleniumTestContext
 import org.gmock.WithGMock
 import org.junit.Before
 import org.junit.Test
-import static grails.plugins.selenium.events.EventHandler.*
+import static grails.plugins.selenium.events.EventHandler.EVENT_TEST_SUITE_END
+import static grails.plugins.selenium.events.EventHandler.EVENT_TEST_SUITE_START
+import static org.junit.Assert.assertFalse
+import static org.junit.Assert.assertTrue
 
 @WithGMock
 class SeleniumServerRunnerTests {
@@ -48,14 +51,31 @@ selenium {
 	void doesNotStartServerIfNotRunningSeleniumSuite() {
 		play {
 			runner.onEvent(EVENT_TEST_SUITE_START, "unit")
+			assertFalse "Selenium server should not be running", isServerRunning()
 			runner.onEvent(EVENT_TEST_SUITE_END, "unit")
+			assertFalse "Selenium server should not be running", isServerRunning()
 		}
 	}
 
-	@Test void startsAndStopsServer() {
+	@Test
+	void startsAndStopsServer() {
 		play {
 			runner.onEvent(EVENT_TEST_SUITE_START, "selenium")
+			assertTrue "Selenium server should be running", isServerRunning()
 			runner.onEvent(EVENT_TEST_SUITE_END, "selenium")
+			assertFalse "Selenium server should not be running", isServerRunning()
+		}
+	}
+
+	private boolean isServerRunning() {
+		def socket = null
+		try {
+			socket = new Socket("localhost", 4444)
+			return socket.isConnected()
+		} catch (ConnectException e) {
+			return false
+		} finally {
+			socket?.close()
 		}
 	}
 
