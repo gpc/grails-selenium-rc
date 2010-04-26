5fa7c60297e9b4c6787b7debabafb0fdc32afcf1
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-04-26 18:14:26 +0100

    moved plugin dependencies for test project into BuildConfig, updated todo & docs

--- TODO.md
+++ TODO.md
@@ -1,8 +1,10 @@
 ## Bugs
 * Path for screenshots is wrong
+* Sort out dependency mess - build is the wrong scope but weird stuff happens with transient dependencies otherwise
 
 ## Testing
 * Test against different Grails versions 1.2.0+
+* Test with Jetty plugin
 
 ## Enhancements
 * Support fixtures plugin via a controller and custom commands?
--- test/projects/selenium-test/grails-app/conf/BuildConfig.groovy
+++ test/projects/selenium-test/grails-app/conf/BuildConfig.groovy
@@ -1,23 +1,29 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir	= "target/test-reports"
+grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
-    inherits "global"
-    log "warn"
-    repositories {        
-        grailsPlugins()
-        grailsHome()
+	inherits "global"
+	log "warn"
+	repositories {
+		grailsPlugins()
+		grailsHome()
 		grailsCentral()
 		mavenLocal()
 		mavenCentral()
-    }
-    dependencies {
+	}
+	dependencies {
 		test("org.hamcrest:hamcrest-all:1.1") {
 			excludes "jmock", "easymock", "junit"
 		}
-    }
-
+	}
+	plugins {
+		compile ":acegi:0.5.3"
+		runtime ":hibernate:1.3.0.RC2"
+		runtime ":tomcat:1.3.0.RC2"
+		test ":build-test-data:1.1.0"
+		test ":spock:0.4-SNAPSHOT"
+	}
 }
 
 grails.plugin.location."selenium-rc" = "../../.."
