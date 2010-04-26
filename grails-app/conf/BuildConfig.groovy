a61a7b8643e2029d244d6240e8578d3edfa5c913
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-04-26 21:53:34 +0100

    correct java client dependency

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -11,7 +11,9 @@ grails.project.dependency.resolution = {
 		mavenCentral()
 	}
 	dependencies {
-		build "org.seleniumhq.selenium.client-drivers:selenium-java-client-driver:1.0.2"
+		test("org.seleniumhq.selenium.client-drivers:selenium-java-client-driver:1.0.2") {
+			excludes "junit", "easymock", "easymockclassextension", "gmaven-runtime-default", "selenium-server"
+		}
 		test("org.gmock:gmock:0.8.0") {
 			excludes "junit"
 			exported = false
