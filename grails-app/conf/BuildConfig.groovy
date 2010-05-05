3d2cab12f0a3d799ee16b08b278cbc7f2d0f936c
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-05-05 12:37:06 +0100

    remove commented out crap from BuildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,6 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
 	inherits "global"
 	log "warn"
@@ -11,9 +10,6 @@ grails.project.dependency.resolution = {
 		mavenCentral()
 	}
 	dependencies {
-//		provided(group: "org.seleniumhq.selenium.server", name: "selenium-server", version: "1.0.3") {
-//			excludes "ant", "commons-codec", "commons-logging", "servlet-api"
-//		}
 		test("org.seleniumhq.selenium.client-drivers:selenium-java-client-driver:1.0.2") {
 			excludes "selenium-server", "gmaven-runtime-default", "junit"
 		}
